a164ded692791a664d9db37252e63f67782af0f2
Cleveland Moses <101227546+JGxEan@users.noreply.github.com>
2025-04-14 23:15:23 -0400

    Created the skeleton layout and functions

---
+++ js.js
@@ -0,0 +1,25 @@
+let x;
+let y;
+let z;
+
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
+
+
+function operate(x, y, operator) {
+  return operator(x, y);
+}
+
